ff89733b4689f516d518adfe547711cc4e268025
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-08-31 22:03:32 -0300

    Add validations and associations to Cart, CartItem, and Product models; implement total price calculation in Cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,12 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  delegate :name, :price, to: :product, prefix: false
+
+  def total_price
+    product.price * quantity
+  end
 end
