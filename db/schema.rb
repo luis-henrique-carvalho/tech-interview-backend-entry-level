819dbb536fb276785ea1345d1dcbfeaf0022b5a5
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-09-04 20:48:21 -0300

    Update database configuration for development and test environments; add last_interaction_at column to carts table with default timestamp.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_01_022015) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_04_234452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_01_022015) do
     t.datetime "updated_at", null: false
     t.datetime "abandoned_at"
     t.string "session_id"
+    t.datetime "last_interaction_at", default: -> { "CURRENT_TIMESTAMP" }
     t.index ["abandoned_at"], name: "index_carts_on_abandoned_at"
     t.index ["session_id"], name: "index_carts_on_session_id"
   end
